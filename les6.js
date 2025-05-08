a11f30086d1b02d64acddc65f6686235b5ccd91f
NickKazban <mykola.kazban@gmail.com>
2025-05-08 21:06:09 +0300

    Float and Math operstion

--- les6.js
+++ les6.js
@@ -1,5 +1,36 @@
 ////Функціяя виконується одразу
-(function Exp(){
+/*(function Exp(){
     console.log('Great jobs');
 })();
+*/
+// Перевірка результату на число /////
 
+/*console.log(isNaN(5));
+console.log(isNaN(undefined)); // Цікаво. Все тому що цей тип не можна конвертувати в число
+console.log(isNaN(NaN));
+console.log(isNaN("qwerty"));*/
+
+//Хотілось би також розібрати бібліотеку Math
+//Округлення
+/*console.log("floor: из 3.6 до " + Math.floor(3.6)); //вниз
+console.log('ceil: из 3.1 до ' + Math.ceil(3.1)); //вгору
+console.log('round: из 3.4 до ' + Math.round(3.4)); //до найближчого
+console.log('round: из 3.7 до ' + Math.round(3.7)); //до найближчого*/
+
+//заокруглення до заданого знака.
+/*var n = 12.664676646766467664676646766467;
+var str = n.toFixed(3); //скільки знаків після коми залишити
+console.log(n + ' => ' + str);*/
+
+//Генератор случайных чисел.
+/*var rnd = Math.random();
+console.log('rnd=' + rnd);
+console.log('rnd=' + rnd.toFixed(2));*/
+
+//Більш практична задача: ціле випадкове число від 1 до 10:
+
+var rnd = Math.random();
+rnd *= 10;
+console.log(rnd)
+rnd = Math.round(rnd);
+console.log(rnd);
